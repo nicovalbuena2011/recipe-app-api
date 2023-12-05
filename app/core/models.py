92af03e17c356769f39498851896b2ff6e342b9c
nicovalbuena2011 <nicovalbuena2011@gmail.com>
2023-12-05 15:42:03 -0500

    creating recipe model, and test for that model

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 """
 Database models.
 """
-
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -46,3 +46,18 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email' # Field for aunthentication
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+    )
+    title = models.CharField('title', max_length=50)
+    description = models.TextField(blank = True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits = 5, decimal_places = 2)
+    link = models.CharField('link', max_length = 50, blank = True)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
